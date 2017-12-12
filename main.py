e789a9fcff19063448c64af428715b3e43038e77
Vysakh Induchoodan <vy.ind@protonmail.com>
2017-12-12 17:45:50 +0530

    Fix QThread Bug

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
-from videoprocess import *
+# from videoprocess import *
+import cv2
+import numpy as np
 from PyQt5.QtGui import QImage
-from PyQt5.QtCore import QUrl, QObject, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import QUrl, QObject, pyqtSignal, pyqtSlot, QThread
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQuick import QQuickView, QQuickPaintedItem, QQuickImageProvider
 from PyQt5.QtQml import QQmlApplicationEngine
@@ -10,13 +12,67 @@ from PyQt5.QtQml import QQmlApplicationEngine
 class processWindow(QObject) :
     def __init__(self) :
         QObject.__init__(self)
-        vid = vprocess("sources/sample.mp4")
 
-    ProcessCompleted = pyqtSignal(arguments=['process'])
-
+    processCompleted = pyqtSignal(int, arguments=['done'])
+    
     @pyqtSlot()
     def process(self) :
-        self.ProcessCompleted.emit()
+        # vid = vprocess("sources/sample.mp4")
+        self.busy = busyThread()
+        self.thread = QThread(self)
+        self.busy.threadCompleted.connect(self.done)
+        self.busy.moveToThread(self.thread)
+        self.thread.started.connect(self.busy.do_work)
+        self.thread.start()
+        # self.busy.finished.connect(self.done)
+        # self.ProcessCompleted.connect(self.busy, SIGNAL("signal"), self.done)
+        # self.busy.start()
+
+    
+    def done(self) :
+        self.processCompleted.emit(1)
+
+
+class busyThread(QObject) :
+    def __init__(self) :
+        QObject.__init__(self)
+
+    threadCompleted = pyqtSignal()
+    
+    @pyqtSlot()
+    def do_work(self) :
+        # vid = vprocess("souces/gait.mp4")
+
+    ########################
+        self.cap = cv2.VideoCapture("sources/gait.mp4")
+        self.frame_width = int(self.cap.get(3))
+        self.frame_height = int(self.cap.get(4))
+
+        self.out_original = cv2.VideoWriter('cache/original.avi', cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 30, (self.frame_width, self.frame_height))
+        self.out_detect = cv2.VideoWriter('cache/detect.avi', cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 30, (self.frame_width, self.frame_height))
+
+        self.fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+
+        while True :
+
+            status, frame = self.cap.read()
+
+            if not status :
+                break
+
+            fgmask = self.fgbg.apply(frame)
+            
+            self.out_original.write(frame)
+            self.out_detect.write(cv2.merge([fgmask, fgmask, fgmask]))
+
+        print("processing done!")
+        self.cap.release()
+        self.out_detect.release()
+        self.out_original.release()
+    ########################
+        self.threadCompleted.emit()
+        # print("done")
+        # self.emit(SIGNAL("signal"),"completed")
 
 
 # Main Function
@@ -26,8 +82,8 @@ if __name__ == '__main__':
     # Create a label and set its properties
     engine = QQmlApplicationEngine()
 
-    win = processWindow()
-    engine.rootContext().setContextProperty("videoanalyze", win)
+    videoanalyze = processWindow()
+    engine.rootContext().setContextProperty("videoanalyze", videoanalyze)
     # engine.addImageProvider("OCV", win)
     engine.load('main.qml')
     # win = engine.rootObjects()[0]
